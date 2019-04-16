6e2ec58a6bca6c5608615a769dbc4012e20d2002
ttexan1 <ttexan.developper@gmail.com>
2019-04-17 01:43:15 +0900

    rails new template_app -m https://raw.githubusercontent.com/ttexan1/rails_template/master/template.rb

---
+++ Gemfile
@@ -0,0 +1,56 @@
+source 'https://rubygems.org'
+
+git_source(:github) do |repo_name|
+  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
+  "https://github.com/#{repo_name}.git"
+end
+
+gem 'rails', '~> 5.2.2'
+gem 'pg', '~> 1.3.6'
+gem 'puma', '~> 3.7'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.2'
+gem 'jquery-rails'
+gem 'jbuilder', '~> 2.5'
+gem 'therubyracer', platforms: :ruby
+gem 'bootstrap-sass'
+gem 'simple_form'
+gem 'html5_validators'
+gem 'kaminari'
+gem 'seed-fu', '~> 2.3'
+gem 'bootsnap', '>= 1.1.0', require: false
+gem 'slim-rails'
+
+group :development, :test do
+  gem 'byebug', platform: :mri
+  gem 'annotate'
+  gem 'pry-rails'
+  gem 'pry-coolline'
+  gem 'pry-byebug'
+  gem 'capybara', '~> 2.13'
+  gem 'selenium-webdriver'
+  gem 'html2slim'
+  gem 'rspec-rails'
+  gem 'spring-commands-rspec'
+  gem 'guard-rspec'
+  gem 'rb-fsevent'
+  gem 'factory_bot_rails'
+  gem 'database_rewinder'
+  gem 'timecop'
+end
+
+group :development do
+  gem 'web-console', '>= 3.3.0'
+  gem 'listen', '~> 3.0.5'
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'bullet'
+  gem 'html2slim'
+end
+
+group :test do
+  gem 'shoulda-matchers'
+end
+
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ config/application.rb
@@ -0,0 +1,43 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module TemplateApp
+  class Application < Rails::Application
+
+    # Set timezone
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    # generatorの設定
+    config.generators do |g|
+      g.orm :active_record
+      g.template_engine :slim
+      g.test_framework  :rspec, :fixture => true
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+      g.view_specs false
+      g.controller_specs false
+      g.routing_specs false
+      g.helper_specs false
+      g.request_specs false
+      g.assets false
+      g.helper false
+    end
+    # 日本語化
+    I18n.available_locales = [:en, :ja]
+    I18n.enforce_available_locales = true
+    # libファイルの自動読み込み
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+          # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
